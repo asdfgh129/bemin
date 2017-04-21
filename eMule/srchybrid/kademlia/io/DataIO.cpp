3b71acb04b9c345eab36f20904f08f8ee2759bd3
bemin <bemin@126.com>
2017-04-21 22:52:43 +0800

    Signed-off-by: bemin <bemin@126.com>

--- eMule/srchybrid/kademlia/io/DataIO.cpp
+++ eMule/srchybrid/kademlia/io/DataIO.cpp
@@ -582,73 +582,78 @@ int KadTagStrCompareNoCase(LPCWSTR dst, LPCWSTR src)
     return (int)(d - s);
 }
 ///snow:<--------------------------------------------------------------------------------------------add by snow
-CString PrintKagTags(TagList * pTags)
+CString PrintKagTags(TagList * plistInfo)
 {
-	CKadTag* pTag;
-	char *pcName = NULL;
-	byte byType = 0;
-	uint16 uLenName = 0;
 	CString strOutput;
 	strOutput.Format(_T("Tags count:%i\n"),pTags->size());
-	for (TagList::const_iterator itTagList = pTags->begin(); itTagList != pTags->end(); ++itTagList)
+
+///snow:处理回应报文中的TagList信息
+	for (TagList::const_iterator itTagList = plistInfo->begin(); itTagList != plistInfo->end(); ++itTagList)
 	{
-		pTag = *itTagList;
-		uint8 uType;
-		if (pTag->m_type == 0xFE)
+		CKadTag* pTag = *itTagList;
+
+		if (!pTag->m_name.Compare(TAG_FILENAME))
 		{
-			if (pTag->GetInt() <= 0xFF)
-				uType = TAGTYPE_UINT8;
-			else if (pTag->GetInt() <= 0xFFFF)
-				uType = TAGTYPE_UINT16;
-			else if (pTag->GetInt() <= 0xFFFFFFFF)
-				uType = TAGTYPE_UINT32;
+			// Set flag based on last tag we saw.
+			strOutput.AppendFormat(_T("Filename:%s\n"),pTag->GetStr());			
+		}
+		else if (!pTag->m_name.Compare(TAG_FILESIZE))
+		{
+			uint64 uSize = 0;
+			if(pTag->IsBsob() && pTag->GetBsobSize() == 8)
+				uSize = *((uint64*)pTag->GetBsob());
 			else
-				uType = TAGTYPE_UINT64;
+				uSize = pTag->GetInt();
+
+			strOutput.AppendFormat(_T("FileSize:%I64u  |  "),uSize);
+
 		}
-		else
-			uType = pTag->m_type;
-
-		//byType = ReadByte();
-		//uLenName = ReadUInt16();
-		pcName = new char[uLenName+1];
-		pcName[uLenName] = 0;
-		//ReadArray(pcName, uLenName);
-
-		switch (uType)
+		else if (!pTag->m_name.Compare(TAG_FILETYPE))
+			strOutput.AppendFormat(_T("FileType:%s  |  "),pTag->GetStr());
+		else if (!pTag->m_name.Compare(TAG_FILEFORMAT))
+			strOutput.AppendFormat(_T("FileFormat:%s  |  "),pTag->GetStr());
+		else if (!pTag->m_name.Compare(TAG_MEDIA_ARTIST))
+			strOutput.AppendFormat(_T("MEDIA_ARTIST:%s  |  "),pTag->GetStr());
+		else if (!pTag->m_name.Compare(TAG_MEDIA_ALBUM))
+			strOutput.AppendFormat(_T("MEDIA_ALBUM:%s  |  "), pTag->GetStr());
+		else if (!pTag->m_name.Compare(TAG_MEDIA_TITLE))
+			strOutput.AppendFormat(_T("MEDIA_TITLE:%s  |  "),pTag->GetStr());
+		else if (!pTag->m_name.Compare(TAG_MEDIA_LENGTH))
+			strOutput.AppendFormat(_T("MEDIA_LENGTH:%i  |  "), (uint32)pTag->GetInt();
+		else if (!pTag->m_name.Compare(TAG_MEDIA_BITRATE))
+			strOutput.AppendFormat(_T("MEDIA_BITRATE:%i  |  "), (uint32)pTag->GetInt();
+		else if (!pTag->m_name.Compare(TAG_MEDIA_CODEC))
+			strOutput.AppendFormat(_T("MEDIA_CODEC:%s  |  "), pTag->GetStr();
+		else if (!pTag->m_name.Compare(TAG_SOURCES))
 		{
-		case TAGTYPE_HASH:
-			// Do NOT use this to transfer any tags for at least half a year!!
-			WriteHash(pTag->GetHash());
-			ASSERT(0);
-			break;
-		case TAGTYPE_STRING:
-			{
-				CUnicodeToUTF8 utf8(pTag->GetStr());
-				WriteUInt16((uint16)utf8.GetLength());
-				WriteArray(utf8, utf8.GetLength());
-				break;
-			}
-		case TAGTYPE_UINT64:
-			WriteUInt64(pTag->GetInt());
-			break;
-		case TAGTYPE_UINT32:
-			WriteUInt32((uint32)pTag->GetInt());
-			break;
-		case TAGTYPE_UINT16:
-			WriteUInt16((uint16)pTag->GetInt());
-			break;
-		case TAGTYPE_UINT8:
-			WriteUInt8((uint8)pTag->GetInt());
-			break;
-		case TAGTYPE_FLOAT32:
-			WriteFloat(pTag->GetFloat());
-			break;
-		case TAGTYPE_BSOB:
-			WriteBsob(pTag->GetBsob(), pTag->GetBsobSize());
-			break;
+			// Some rouge client was setting a invalid availability, just set it to 0
+			strOutput.AppendFormat(_T("SOURCES:%i  |  "), (uint32)pTag->GetInt();
+			if( uAvailability > 65500 )
+				uAvailability = 0;
 		}
-	}
-		
+		else if (!pTag->m_name.Compare(TAG_PUBLISHINFO))
+		{
+			if (uFromKadVersion >= KADEMLIA_VERSION6_49aBETA)
+			{
+				// we don't keep this as tag, but as a member property of the searchfile, as we only need its informations
+				// in the search list and don't want to carry the tag over when downloading the file (and maybe even wrongly publishing it)
+				strOutput.AppendFormat(_T("PUBLISHINFO:%i  |  "), (uint32)pTag->GetInt();
+/*#ifdef _DEBUG
+				uint32 byDifferentNames = (uPublishInfo & 0xFF000000) >> 24;
+				uint32 byPublishersKnown = (uPublishInfo & 0x00FF0000) >> 16;
+				uint32 wTrustValue = uPublishInfo & 0x0000FFFF;
+				DebugLog(_T("Received PublishInfoTag: %u different names, %u Publishers, %.2f Trustvalue"), byDifferentNames, byPublishersKnown, (float)wTrustValue / 100.0f);  
+#endif*/	
+			}
+
+		}
+		else if (!pTag->m_name.Compare(TAG_KADAICHHASHRESULT))
+		{
+			strOutput.AppendFormat(_T("KADAICHHASHRESULT:暂略  |  ")
+		}
+		delete pTag;
+	}  ///snow:end for  报文中的Taglist信息处理完毕
+			
 	return strOutput;
 }
 ///snow:---------------------------------------------------------------------->add by snow
